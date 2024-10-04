d830ba4198803cc2acea4bf946c4969b31185eae
Sneha Rajaraman <sr934@cornell.edu>
2024-10-04 14:10:15 -0400

    completed onboarding

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,26 +1,26 @@
 import React from "react";
 
 /** A simple Button component */
-const Button = () => {
-  return <button>Press me</button>;
-};
+// const Button = () => {
+//   return <button>Press me</button>;
+// };
 
-export default Button;
+// export default Button;
 
 // // Uncomment for Button Prop Onboarding
 // import React from "react";
 
-// /**
-//  * Component for a general button
-//  * @param text is the text on the button
-//  */
+/**
+ * Component for a general button
+ * @param text is the text on the button
+ */
 
-// interface Props {
-//   text: string;
-// }
+interface Props {
+  text: string;
+}
 
-// const Button = ({ text }: Props) => {
-//   return <button>{text}</button>;
-// };
+const Button = ({ text }: Props) => {
+  return <button>{text}</button>;
+};
 
-// export default Button;
+export default Button;
--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -1,25 +1,45 @@
-import React from "react";
+// import React from "react";
 
 /** An About page */
-const About = () => {
-  return <>Hello there</>;
-};
-
-export default About;
-
-// // Uncomment for Button Onboarding
-// import React from "react";
-// import Button from "@/components/Button";
-
-// /** An About page */
 // const About = () => {
-//   return (
-//     <>
-//       <Button />
-//       {/* <Button text="hello" /> */}
-//       Hello there
-//     </>
-//   );
+//   return <>Hello there</>;
 // };
 
 // export default About;
+
+// // Uncomment for Button Onboarding
+import React from "react";
+import Button from "@/components/Button";
+import LineGraph from "@/components/charts/line-graph";
+const data = [
+  { x: 1.0, y: 9.0 },
+  { x: 1.5, y: 6.0 },
+  { x: 2.5, y: 4.0 },
+  { x: 4.0, y: 2.0 },
+  { x: 5.0, y: 1.6 },
+  { x: 6.0, y: 2.4 },
+  { x: 7.0, y: 3.0 },
+  { x: 8.0, y: 3.4 },
+  { x: 9.0, y: 3.6 },
+];
+
+/** An About page */
+const About = () => {
+  return (
+    <>
+      <Button text="Hello" />
+      <br />
+      {/* <Button text="hello" /> */}
+      Hello there
+      <br />
+      <br />
+      <br />
+      <pre>
+        <b>Line Graph:</b>
+      </pre>
+      <LineGraph info={data} />
+    </>
+  );
+};
+
+export default About;
