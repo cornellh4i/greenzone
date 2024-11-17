0c4579c4ec93f05b19c546aa340a90cdca4766e1
DannyThorne01 <danielfrancais592@gmail.com>
2024-11-17 12:12:25 +0000

    Updated SidePanel

--- frontend/src/pages/sidepanelpage.tsx
+++ frontend/src/pages/sidepanelpage.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import SidePanel from "@/components/organisms/SidePanel";
-import SidePain from "@/components/organisms/SidePain";
 
 /** An About page */
 
 const SidePanelPage = () => {
   return (
     <div>
-      {/*<SidePanel></SidePanel>*/}
-      <SidePain></SidePain>
+      <SidePanel></SidePanel>
     </div>
   );
 };
