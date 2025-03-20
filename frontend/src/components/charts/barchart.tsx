3ae6739c38d075e03bec06a53c77f1e08f7ffaa6
Katherine <katherine.y.hu@gmail.com>
2025-03-20 17:04:16 -0400

    aligned graphs in SidePanel

--- frontend/src/components/charts/barchart.tsx
+++ frontend/src/components/charts/barchart.tsx
@@ -4,9 +4,10 @@ import * as d3 from "d3";
 export interface Props {
   datasets: { aimag: string; data: { x: string; y: number }[] }[];
   livestock: string[];
+  orientation: boolean; //true - vertical, false - horizontal
 }
 
-const BarChart: React.FC<Props> = ({ datasets, livestock }) => {
+const BarChart: React.FC<Props> = ({ datasets, livestock, orientation}) => {
   const svgRef = useRef<SVGSVGElement | null>(null);
 
   useEffect(() => {
@@ -37,27 +38,40 @@ const BarChart: React.FC<Props> = ({ datasets, livestock }) => {
       .style("background-color", "#ffffff");
 
     // xScale based on livestock types
-    const xScale = d3
+    const xScaleVert = d3
       .scaleBand()
       .domain(livestock)  // Livestock types as x-axis labels
-      .range([m.left, w - m.right])
-      .padding(0.2);
+      .range([m.left, w -m.right])
+      .padding(0.2) ;
 
     // yScale based on total livestock counts
-    const yScale = d3
+    const yScaleVert = d3
       .scaleLinear()
       .domain([0, d3.max(aggregatedData, d => d.totalLivestockCount) || 0])
       .range([h - m.bottom, m.top]);
 
-    // Gridlines
-    const makeYGridlines = () => d3.axisLeft(yScale).ticks(5);
+    // yScale based on livestock types
+    const yScaleHorizontal = d3
+      .scaleBand()
+      .domain(livestock)  // Livestock types as x-axis labels
+      .range([m.top, h-m.bottom])
+      .padding(0.2) ;
+
+    // xScale based on total livestock counts
+    const xScaleHorizontal = d3
+      .scaleLinear()
+      .domain([0, d3.max(aggregatedData, d => d.totalLivestockCount) || 0])
+      .range([m.left, w - m.right]);
+
+    // // Gridlines
+    // const makeYGridlines = () => {return orientation ? d3.axisLeft(yScaleVert).ticks(5) : d3.axisBottom(xScaleHorizontal).ticks(5)};
 
     // Draw x-axis
     d3svg.append("g")
       .attr("transform", `translate(0, ${h - m.bottom})`)
-      .call(d3.axisBottom(xScale));
+      .call(orientation ? d3.axisBottom(xScaleVert): d3.axisBottom(xScaleHorizontal));
 
-    // Add x-axis label
+    //Add x-axis label
     d3svg
       .append("text")
       .attr("class", "x-axis-label")
@@ -65,12 +79,12 @@ const BarChart: React.FC<Props> = ({ datasets, livestock }) => {
       .attr("x", w / 2)
       .attr("y", h - 10)
       .style("font-size", "14px")
-      .text("Types of Livestock");
+      .text(orientation ? "Types of Livestock" : "# of Livestock");
 
     // Draw y-axis
     d3svg.append("g")
       .attr("transform", `translate(${m.left}, 0)`)
-      .call(d3.axisLeft(yScale));
+      .call(orientation ? d3.axisLeft(yScaleVert) : d3.axisLeft(yScaleHorizontal));
 
     // Add y-axis label
     d3svg
@@ -81,21 +95,21 @@ const BarChart: React.FC<Props> = ({ datasets, livestock }) => {
       .attr("x", -h / 2)
       .attr("y", 20)
       .style("font-size", "14px")
-      .text("# of Livestock");
+      .text(orientation ? "# of Livestock" :  "Types of Livestock");
 
-    // Add gridlines
-    d3svg
-      .append("g")
-      .attr("class", "grid")
-      .attr("transform", `translate(${m.left}, 0)`)
-      .call(
-        makeYGridlines()
-          .tickSize(-w + m.left + m.right)
-          .tickFormat(() => "")
-      )
-      .style("stroke-dasharray", "3,3")
-      .style("stroke", "#e0e0e0")
-      .style("stroke-opacity", "0.3");
+    // // Add gridlines
+    // d3svg
+    //   .append("g")
+    //   .attr("class", "grid")
+    //   .attr("transform", orientation ? `translate(${m.left}, 0)` : `translate(0, ${h-m.bottom})`)
+    //   .call(
+    //     makeYGridlines()
+    //       .tickSize(orientation ? -w + m.left + m.right : -h + m.top + m.bottom)
+    //       .tickFormat(() => "")
+    //   )
+    //   .style("stroke-dasharray", "3,3")
+    //   .style("stroke", "#e0e0e0")
+    //   .style("stroke-opacity", "0.3");
 
     // Draw bars
     d3svg.selectAll(".bar")
@@ -103,13 +117,25 @@ const BarChart: React.FC<Props> = ({ datasets, livestock }) => {
       .enter()
       .append("rect")
       .attr("class", "bar")
-      .attr("x", d => xScale(d.livestockType)!)
-      .attr("y", d => yScale(d.totalLivestockCount))
-      .attr("width", xScale.bandwidth())
-      .attr("height", d => yScale(0) - yScale(d.totalLivestockCount))
-      .attr("fill", "#69b3a2");
+      .attr("x", d => orientation ? xScaleVert(d.livestockType)! :  m.left)
+      .attr("y", d => orientation ? yScaleVert(d.totalLivestockCount) : yScaleHorizontal(d.livestockType)!)
+      .attr("width", orientation ? xScaleVert.bandwidth() :  d => xScaleHorizontal(d.totalLivestockCount) - xScaleHorizontal(0))
+      .attr("height", orientation ? d => yScaleVert(0) - yScaleVert(d.totalLivestockCount) : yScaleHorizontal.bandwidth())
+      .attr("fill", "#205142");
 
-  }, [datasets, livestock]);
+    d3svg.selectAll(".label")
+      .data(aggregatedData)
+      .enter()
+      .append("text")
+      .attr("class", "label")
+      .attr("x",d => orientation ? xScaleVert(d.livestockType)! + xScaleVert.bandwidth() / 2 : xScaleHorizontal(d.totalLivestockCount)! + 40)
+      .attr("y", d => orientation 
+        ? yScaleVert(d.totalLivestockCount) - 5 : yScaleHorizontal(d.livestockType)! + yScaleHorizontal.bandwidth() / 2)
+      .attr("text-anchor", "middle")
+      .attr("alignment-baseline", "middle")
+      .text(d=> Math.floor(d.totalLivestockCount));
+
+  }, [datasets, livestock, orientation]);
 
   return (
     <div className="barchart" style={{ width: "100%", height: "100vh", maxHeight: "500px", maxWidth: "600px" }}>
